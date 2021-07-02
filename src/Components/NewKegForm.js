5e86e964e91e40378188bcaf035749ec15048f1e
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-02 11:14:33 -0700

    add add form

---
+++ src/Components/NewKegForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { v4 } from 'uuid';
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewKegForm(props){
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, style: event.target.style.value, price: event.target.price.value, abv: event.target.abv.value, id: v4()});
+  }
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleNewKegFormSubmission}
+        buttonText="Add Keg" />
+    </React.Fragment>
+  );
+}
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+};
+
+export default NewKegForm; 
