733268d11c52e70ede7edf342e2d123618565d88
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-07-09 11:50:07 -0700

    fix test

--- __tests__/reducers/keg-list-reducer.test.js
+++ __tests__/reducers/keg-list-reducer.test.js
@@ -20,13 +20,13 @@ describe('kegListReducer', () => {
 
   const currentState = {
     1: {
-    name: 'mannys',
-    brand: 'georgetown',
-    style: 'pale ale',
-    price: 5,
-    abv: '5.4%',
-    pints: 124,
-    id: 1 
+      name: 'mannys',
+      brand: 'georgetown',
+      style: 'pale ale',
+      price: 5,
+      abv: '5.4%',
+      pints: 124,
+      id: 1 
     },
     2: {
       name: 'rogers',
@@ -35,7 +35,7 @@ describe('kegListReducer', () => {
       price: 5,
       abv: '4.9%',
       pints: 124,
-    id: 2 
+      id: 2 
     }
   }
 
@@ -58,9 +58,12 @@ describe('kegListReducer', () => {
 
     expect(kegListReducer({}, action)).toEqual({
       [id] : {
-        names: names,
-        location: location,
-        issue: issue,
+        name: name,
+        brand: brand,
+        style: style,
+        price: price,
+        abv: abv,
+        pints: pints,
         id: id
       }
     });
@@ -72,15 +75,20 @@ describe('kegListReducer', () => {
       id: 1
     };
     expect(kegListReducer(currentState, action)).toEqual({
-      2: {names: 'Jasmine and Justine',
-        location: '2a',
-        issue: 'Reducer has side effects.',
-        id: 2 }
+      2: {
+        name: 'rogers',
+        brand: 'georgetown',
+        style: 'pilsner',
+        price: 5,
+        abv: '4.9%',
+        pints: 124,
+        id: 2 
+      }
     });
   });
 
   test('Should add a formatted wait time to keg entry', () => {
-    const { names, location, issue, timeOpen, id } = kegData;
+    const { name, brand, style, price, abv, pints, timeOpen, id } = kegData;
     action = {
       type: c.UPDATE_TIME,
       formattedWaitTime: '4 minutes',
@@ -88,9 +96,12 @@ describe('kegListReducer', () => {
     };
     expect(kegListReducer({ [id] : kegData }, action)).toEqual({
       [id] : {
-        names: names,
-        location: location,
-        issue: issue,
+        name: name,
+        brand: brand,
+        style: style,
+        price: price,
+        abv: abv,
+        pints: pints,
         timeOpen: timeOpen,
         id: id,
         formattedWaitTime: '4 minutes'
@@ -99,21 +110,27 @@ describe('kegListReducer', () => {
   });
 
   test('should successfully add a keg to the keg list that includes Moment-formatted wait times', () => {
-    const { names, location, issue, timeOpen, id } = kegData;
+    const { name, brand, style, price, abv, pints, timeOpen, id } = kegData;
     action = {
       type: 'ADD_KEG',
-      names: names,
-      location: location,
-      issue: issue,
+      name: name,
+      brand: brand,
+      style: style,
+      price: price,
+      abv: abv,
+      pints: pints,
       timeOpen: timeOpen,
       id: id,
       formattedWaitTime: new Moment().fromNow(true)
     };
     expect(kegListReducer({}, action)).toEqual({
       [id] : {
-        names: names,
-        location: location,
-        issue: issue,
+        name: name,
+        brand: brand,
+        style: style,
+        price: price,
+        abv: abv,
+        pints: pints,
         timeOpen: timeOpen,
         id: id,
         formattedWaitTime: 'a few seconds'
